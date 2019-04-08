ae4c6161bffdb918e329e49b1d7f690cfd21905a
Primo <primo.chmiel@gmail.com>
2019-04-08 20:03:33 +0100

    javascript changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log('dziala')
+
+items = ['1','2' ,'3' , '4' , '5' , '6' , '7'];
+const dwapierwsze = items.slice(0,2);
+console.log(dwapierwsze);
+
+const animals =  ['dog', 'cat', 'canary', 'mouse'];
+console.log(animals)
+animals.splice(1,0, 'hamster')
+console.log(animals)
