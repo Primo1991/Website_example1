f804d84ab6e63850a3633aab887ae08e9abec59c
Primo1991 <dev@przemyslawchmiel.com>
2019-04-16 00:01:33 +0100

    Set - ES6 practice

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
-console.log('dziala')
+// console.log('dziala')
 
-items = ['1','2' ,'3' , '4' , '5' , '6' , '7'];
-const dwapierwsze = items.slice(0,2);
-console.log(dwapierwsze);
+// items = ['1','2' ,'3' , '4' , '5' , '6' , '7'];
+// const dwapierwsze = items.slice(0,2);
+// console.log(dwapierwsze);
 
-const animals =  ['dog', 'cat', 'canary', 'mouse'];
-console.log(animals)
-animals.splice(1,0, 'hamster')
-console.log(animals)
+// const animals =  ['dog', 'cat', 'canary', 'mouse'];
+// console.log(animals)
+// animals.splice(1,0, 'hamster')
+// console.log(animals)
 
-let names = ['Marek', 'Zemek', 'Adam'];
-console.log(names.sort());
+// let names = ['Marek', 'Zemek', 'Adam'];
+// console.log(names.sort());
 
-let numbers = ['3','35','1','12','30'];
-console.log(numbers.sort());
+// let numbers = ['3','35','1','12','30'];
+// console.log(numbers.sort());
 
-let name = 'Marek, Przemek, Adam, Mikolaj, Sebastian'.split(', ', 2);
+// let name = 'Marek, Przemek, Adam, Mikolaj, Sebastian'.split(', ', 2);
+
+
+// window.onload = function(){
+//     var names = new Set();
+//     names.add('Primo').add('Zumika').add('Adam');
+//     names.delete('Adam');  
+//    // names.clear()  
+//     console.log(names.has('Zumika'));
+//     console.log(names.size)
+//     console.log(names)
+//}
+
+
+window.onload = function(){
+    var ninjas = ["Shen", "Zed", "Udyr", "Ahri", "Akali"]
+    var refinedNinjas = new Set(ninjas);
+
+    console.log(refinedNinjas)
+
+    ninjas = [...refinedNinjas]
+
+    console.log(ninjas)
+
+    for(let ninja of refinedNinjas.values())
+    console.log(ninja)
+}
+
